db2a3f0b274c47a224282077fb2e8a101659396d
bl2i4n <bl2i4n@yahoo.com>
2018-01-03 15:40:52 -0800

    feat: add readableAPI to get data from

---
+++ src/utils/readableAPI.js
@@ -0,0 +1,29 @@
+const headers = new Headers({
+  'Authorization': 'jvg'
+});
+
+export const fetchCategories = () => {
+  return fetch(`http://localhost:3001/categories`, {headers})
+    .then((res) => res.json())
+    .then(({categories}) => categories);
+};
+
+export const fetchCategoryPosts = (category) => {
+  return fetch(`http://localhost:3001/${category}/posts`, {headers})
+    .then((rest) => res.json())
+};
+
+export const fetchPosts = () => {
+  return fetch(`http://localhost:3001/posts`, {headers})
+    .then((res) => res.json())
+};
+
+export const fetchPostById = (id) => {
+  return fetch(`http://localhost:3001/posts/${id}`, {headers})
+    .then((rest) => res.json())
+};
+
+export const fetchPostComments = (id) => {
+  return fetch(`http://localhost:3001/posts/${id}/comments`, {headers})
+    .then((res) => res.json())
+};
